a204a9129b0dfcf6fbfcfc0e81811558c14ac45e
elhache <hsaavedrauba@gmail.com>
2024-03-19 01:25:00 -0300

    Proyecto final de lista de tareas con react y typescript

---
+++ src/components/ListaTareas.tsx
@@ -0,0 +1,19 @@
+import { Tarea } from "./Tarea"
+
+type ListaTareas = {
+    listaTareas: string[]
+    borrarTarea: (index: number) => void
+}
+
+export const ListaTareas = ({listaTareas, borrarTarea}: ListaTareas) => {
+  return (
+    <div className="TaskList">
+        {listaTareas.map((tarea,index) => (
+          <Tarea key={index} tarea={tarea} borrarTarea={() => borrarTarea(index)}></Tarea>
+            
+        )
+        )}
+    </div>
+  )
+}
+
